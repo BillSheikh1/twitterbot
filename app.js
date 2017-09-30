6c097674add4d1d5a4c8fb3ee17931da9c7a5114
Bill Sheikh <billsheikh1@googlemail.com>
2017-09-30 15:23:41 +0100

    Final file

--- app.js
+++ app.js
@@ -1,38 +1,94 @@
 var TwitterPackage = require('twitter');
 
-
-
-// importing my secret.json file
-
 var secret = {
-  consumer_key: '3Btk3hzMYKFEWH5Upg0MQ7RR0',
-  consumer_secret: 'qD0wxoK6oFaXWCuzhl3X3MmdcQ1VDQQ4WUCWJGrkmpzrvbMonJ',
-  access_token_key: '911361446675656704-Ncx4qOlTXFLfYa3ptwTinYgawkCev9k',
-  access_token_secret: 'K8Mc9iEiEeS33Si9fJwf9qdm0tBka67SfefLPsyDIqQUO'
+
+  consumer_key: '',
+
+  consumer_secret: '',
+
+  access_token_key: '',
+
+  access_token_secret: ''
+
 }
 
-//make a new Twitter object
+//make a new twitter object
 
 var Twitter = new TwitterPackage(secret);
+var tweets = [];
 
+/**
+ * Generates jokes.
+ */
+var makeJokes = function() {
+  var jokes = require('one-liner-joke');
+  var getRandomJoke = jokes.getRandomJoke();
+  var all = JSON.stringify(getRandomJoke);
+  var allArray = all.split(":");
+  var final = allArray[1].split('"');
+  return final[1];
+}
 
-var post = Twitter.post('statuses/update', {status: 'Tech'}, function(error, tweet, response) {
-  if(error) {
-    console.log(error);
+/**
+ * Removes @'s.
+ */
+var makeTweet = function(tweet) {
+  var finalTweet = "";
+  for(var i = 0; i<tweet.length; i++) {
+    if(tweet[i] != "@") {
+      finalTweet += tweet[i];
+    }
+  }
+  return finalTweet;
+}
+
+/**
+ * Tests the sentiment of the statement.
+ */
+var testSentiment = function(text) {
+  var tweet = makeTweet(text);
+  var sentiment = require('sentiment');
+  var r1 = sentiment(tweet);
+
+  if(r1 > 0) {
+    Twitter.post('statuses/update', {status: tweet}, function(error, tweets, response) {
+      console.log(tweet);
+      if(error) {
+        console.log(error);
+      }
+      tweets = [];
+    });
   }
   else {
-    console.log(tweet);
-    console.log(response);
+    testSentiment(makeJokes());
   }
-});
+}
 
+// Call the stream function and pass in 'statuses/filter', our filter object, and our callback
 
 Twitter.stream('statuses/filter', {track: '#peaceday'}, function(stream) {
+
   stream.on('data', function(tweet) {
+
     console.log(tweet.text);
+    tweets.push(tweet);
+
+    stream.on('error', function(error) {
+
+      console.log(error);
+
+    });
+
   });
 
-  stream.on('error', function(error) {
-    console.log(error);
-  });
 });
+
+var time = setInterval(function() {
+  if(tweets.length != 0) {
+    var random = Math.floor(Math.random() * tweets.length) + 0;
+    testSentiment(tweets[random].text);
+  }
+  else {
+    testSentiment(makeJokes());
+  }
+}, 3600000);
